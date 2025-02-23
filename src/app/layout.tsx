48f15b0988877687f01ec8f24be9df83535a6d08
Amaurys De Los Santos Mendez <amaurysdlsm@gmail.com>
2025-02-23 13:04:21 -0500

    Refactor layout and styling: update global CSS variables, enhance navbar structure, and improve not-found page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,16 @@
-"use client";
-
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./navbar";
-import { usePathname } from "next/navigation";
+import Navbar from "./Navbar";
 import { Toaster } from "./components/ui/sonner";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const pathname = usePathname();
-
-  // Check if we're on the landing page (i.e., home page)
-  const isLandingPage = pathname === "/";
-
   return (
     <html lang="en">
-      <body className="min-h-screen bg-[#1a1b26]">
-        {!isLandingPage && <Navbar />}
-        <main className={`${!isLandingPage ? 'pt-[64px]' : ''}`}>
+      <body className="min-h-screen bg-[#2a35a8]">
+        <Navbar />
+        
+        <main className="">
           {children}
         </main>
         <Toaster />
