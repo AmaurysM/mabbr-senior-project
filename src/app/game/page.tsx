62e0c1e1fa07ab51cb4cc95f369afe6e94b6aa1f
Bryant <143537731+BryantVelasquez@users.noreply.github.com>
2025-04-17 23:56:19 -0400

    Making stock game

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -3,12 +3,10 @@
 import React from "react";
 import StockPredictionGame from "@/app/components/StockPredictionGame";
 
-export default function StockPredictor() {
+export default async function GamePage() {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-6">
-        Stock Prediction Game
-      </h1>
+      <h1 className="text-3xl font-bold text-center mb-6">Stock Prediction Game</h1>
       <StockPredictionGame />
     </div>
   );
